336ed1eb05fd2aec65607f9998ba7937994670c7
Forseti13 <57772293+Forseti13@users.noreply.github.com>
2019-11-14 17:18:49 -0300

    adicionando os scripts

---
+++ Povoamento.sql
@@ -0,0 +1,62 @@
+insert into empregado (nome, endereco, salario, supervisor, depto)
+values ('empregado_A', 'endereco_A', 1000.25,1, 1);
+insert into empregado (nome, endereco, salario, supervisor, depto)
+values ('empregado_B', 'endereco_B', 998,1, 1);
+insert into empregado (nome, endereco, salario, supervisor, depto)
+values ('empregado_C', 'endereco_C', 998,1, 1);
+insert into empregado (nome, endereco, salario, supervisor, depto)
+values ('empregado_D', 'endereco_D', 998,1, 1);
+insert into empregado (nome, endereco, salario, supervisor, depto)
+values ('empregado_E', 'endereco_E', 998,1, 1);
+
+
+
+insert into departamento (nome, gerente, dataini)
+values('departamento_A', 1, '2019/11/14');
+insert into departamento (nome, gerente, dataini)
+values('departamento_B', 1, '2019/11/14');
+insert into departamento (nome, gerente, dataini)
+values('departamento_C', 1, '2019/11/14');
+insert into departamento (nome, gerente, dataini)
+values('departamento_D', 1, '2019/11/14');
+insert into departamento (nome, gerente, dataini)
+values('departamento_E', 1, '2019/11/14');
+
+
+
+insert into projeto (nome, local, depart)
+values ('projeto_A', 'local_A', 1);
+insert into projeto (nome, local, depart)
+values ('projeto_B', 'local_B', 1);
+insert into projeto (nome, local, depart)
+values ('projeto_C', 'local_C', 1);
+insert into projeto (nome, local, depart)
+values ('projeto_D', 'local_D', 1);
+insert into projeto (nome, local, depart)
+values ('projeto_E', 'local_E', 1);
+
+
+
+insert into alocacao (matric, codigop, horas)
+values(1, 1, 30);
+insert into alocacao (matric, codigop, horas)
+values(1, 2, 30);
+insert into alocacao (matric, codigop, horas)
+values(1, 3, 30);
+insert into alocacao (matric, codigop, horas)
+values(1, 4, 30);
+insert into alocacao (matric, codigop, horas)
+values(1, 5, 30);
+
+
+insert into dependente (matricula, nome, sexo)
+values(232, 'nome_A','M');
+insert into dependente (matricula, nome, sexo)
+values(323, 'nome_B','M');
+insert into dependente (matricula, nome, sexo)
+values(133, 'nome_C','M');
+insert into dependente (matricula, nome, sexo)
+values(264, 'nome_D','M');
+insert into dependente (matricula, nome, sexo)
+values(592, 'nome_E','M');
+
